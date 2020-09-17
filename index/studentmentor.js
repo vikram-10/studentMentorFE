d8d7125774f5694b2a46f2c85a5ffe9f9dc8e102
vikithespider@gmail.com <vikrambalaji2k@gmail.com>
2020-09-17 18:58:26 +0530

    added frontEnd

---
+++ index/studentmentor.js
@@ -0,0 +1,58 @@
+var tableContainer=document.createElement('div');
+tableContainer.setAttribute('class','container');
+
+
+var table=document.createElement('table');
+table.setAttribute('class','table table-dark');
+table.innerHTML=`  <thead>
+<tr>
+  <th scope="col">#</th>
+  <th scope="col">Name</th>
+  <th scope="col">Status</th>
+  <th scope="col">ASSIGN</th>
+</tr>
+</thead>`
+
+async function getStudents(){
+    try{
+        let count=1;
+    var studData=await fetch('http://localhost:3000/showStudents');
+    var finalStudData=await studData.json();
+    finalStudData.forEach(element => {
+        if(element.status!="assigned"){
+            var tableRow=document.createElement('tr');
+            var tableHeader=document.createElement('th');
+            tableHeader.setAttribute('scope','row');
+            tableHeader.innerHTML=`${count}`
+            var tableData1=document.createElement('td');
+            tableData1.innerText=`${element.name}`;
+            var tableData2=document.createElement('td');
+            tableData2.innerText=`${element.status}`;
+            var tableData3=document.createElement('button');
+            tableData3.setAttribute('class','btn btn-primary');
+            tableData3.setAttribute('id',`button${count}`);
+            tableData3.innerText=`ASSIGN!`;
+            count++;
+            tableBody.append(tableRow);
+            tableRow.append(tableHeader)
+            tableRow.append(tableData1);
+            tableRow.append(tableData2);
+            tableRow.append(tableData3);
+            tableData3.addEventListener('click',function(){
+                localStorage.setItem('name',`${element.name}`);
+                window.location.href=`../mentorassign/mentorAssign.html`;
+            })
+        }
+    });
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+var tableBody=document.createElement('tbody');
+
+document.body.append(tableContainer)
+tableContainer.append(table);
+table.append(tableBody);
+getStudents();
---
+++ mentorassign/mentorAssign.js
@@ -0,0 +1,80 @@
+var container=document.createElement('div');
+container.setAttribute('class','container');
+
+var heading=document.createElement('h1');
+let userName=localStorage.getItem('name');
+heading.innerText=`Hello ${userName}`;
+
+var heading2=document.createElement('h3');
+heading2.innerText=`List of Mentors:`;
+
+var Mentortable=document.createElement('table');
+Mentortable.setAttribute('class','table table-dark');
+Mentortable.innerHTML=`<thead>
+<tr>
+  <th scope="col">#</th>
+  <th scope="col">Mentor Name</th>
+  <th scope="col">Students Enrolled</th>
+  <th scope="col">Enroll</th>
+</tr>
+</thead>`
+
+let tableBody=document.createElement('tbody');
+
+async function getMentors(){
+    let count=1;
+    var allMentors=await fetch('http://localhost:3000/showMentors');
+    var finalMentorData=await allMentors.json();
+    console.log(finalMentorData);
+    finalMentorData.forEach(element => {
+    let tableRow=document.createElement('tr');
+    let tableHeader=document.createElement('th');
+    tableHeader.setAttribute('scope','row');
+    tableHeader.innerText=`${count}`;
+    let tableData1=document.createElement('td');
+    tableData1.innerText=`${element.name}`
+    tableBody.append(tableRow);
+    tableRow.append(tableHeader);
+    tableRow.append(tableData1);  
+    let tableData2=document.createElement('td');
+    if(element.studentName!=undefined){
+    var enrolledStudents=(element.studentName);
+    enrolledStudents.forEach(element1 => {
+        tableData2.innerText+=`${element1} `;
+    });
+}
+else{
+    tableData2.innerText="None";
+}
+tableRow.append(tableData2);
+    let tableData3=document.createElement('button');
+    tableData3.setAttribute('class','btn btn-primary');
+    tableData3.setAttribute('id',`enrollButton${count}`)
+    tableData3.innerText=`Enroll`;
+    tableRow.append(tableData3);
+    count++;
+    tableData3.addEventListener("click",async function(){
+        let studentName1=localStorage.getItem('name');
+        try{
+        let enrolling=await fetch(`http://localhost:3000/assignStudent/${element._id}/${studentName1}`,{
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+              },
+        });
+    }
+    catch(err){
+        console.log(err);
+    }
+        alert("Enrolled!");
+    })
+    });
+}
+
+document.body.append(container);
+container.append(heading);
+container.append(heading2);
+container.append(Mentortable);
+Mentortable.append(tableBody);
+
+getMentors();
